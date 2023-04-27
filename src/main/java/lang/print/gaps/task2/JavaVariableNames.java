0bd3fbe4de1de7b31f5af7f5bd67550d9f746b2f
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-04-28 01:57:24 +0300

    updated 2 classes

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -3,9 +3,9 @@ package lang.print.gaps.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int tomorrow2 = 0;
-        int money$ = 10;
-        int hundredPercent = 100;
-        int backExclamation = 1000;
+        int money = 10;
+        int hundred = 100;
+        int back = 1000;
         int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
